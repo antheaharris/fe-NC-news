39dab33f53a0f94b238ded96766cde478e7babf2
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-15 15:55:54 +0100

    added functionality setting state to api article data & 'loading' functionality

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,30 +1,45 @@
 import React from "react";
+import * as api from "../api";
 
 class Articles extends React.Component {
   state = {
-    articles: [
-      {
-        title: "Seafood substitutions are increasing",
-        topic: "cooking",
-        author: "weegembump",
-        body: "Text from the article..",
-        created_at: 1527695953341
-      }
-    ]
+    articles: [],
+    loading: true
   };
 
   render() {
-    const { articles } = this.state;
+    const { articles, loading } = this.state;
+    if (loading) {
+      return <p>great articles take time...</p>;
+    }
     return (
       <div>
         <ul>
           {articles.map(article => {
-            return <li>{article.title}</li>;
+            return (
+              <li key={article.title} className="articleCard">
+                <h3>{article.title}</h3>
+                <p>{article.topic}</p>
+                <p>{article.author}</p>
+              </li>
+            );
           })}
         </ul>
       </div>
     );
   }
+
+  componentDidMount = () => {
+    api
+      .getArticles()
+      .then(articles => {
+        console.log(articles);
+        this.setState({ articles, loading: false });
+      })
+      .catch(err => {
+        this.setState({ err });
+      });
+  };
 }
 
 export default Articles;
