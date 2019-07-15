41bd3b956f94bc9ee0e085a5ab2cd44e2a1c99c3
Anthea Harris <antheaharris@antheas-air.lan>
2019-07-15 23:19:09 +0100

    made Articles file to hold component jsx & css files

---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import * as api from "../../api";
+// import { Link } from "@reach/router";
+import ArticleCard from "../ArticleCard/ArticleCard";
+import styles from "./Articles.module.css";
+
+class Articles extends React.Component {
+  state = {
+    articles: [],
+    loading: true
+  };
+
+  render() {
+    const { articles, loading } = this.state;
+    if (loading) {
+      return <p>great articles take time...</p>;
+    }
+    return (
+      <div>
+        <ul className={styles.articlesList}>
+          {articles.map(article => {
+            return <ArticleCard article={article} />;
+          })}
+        </ul>
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    api
+      .getArticles()
+      .then(articles => {
+        this.setState({ articles, loading: false });
+      })
+      .catch(err => {
+        this.setState({ err });
+      });
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.topic !== this.props.topic) {
+      api.getArticles(this.props.topic).then(articles => {
+        this.setState({ articles, loading: false });
+      });
+    }
+  };
+}
+
+export default Articles;
