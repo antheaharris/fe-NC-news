c598cc0383ded04666229ad8e4e66003b9bcc156
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-16 09:21:53 +0100

    built in Link from article cards on articles page to their correspondonding article page

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -4,27 +4,14 @@ import styles from "./ArticleCard.module.css";
 
 const ArticleCard = ({ article }) => {
   return (
-    <li key={article.title} className={styles.articleCard}>
-      <h3>{article.title}</h3>
-      <Link to={`/articles/topics/${article.topic}`}>{article.topic}</Link>
-      <p>{article.author}</p>
-    </li>
+    <Link to={`/articles/${article.article_id}`}>
+      <li key={article.article_id} className={styles.articleCard}>
+        <h3>{article.title}</h3>
+        <Link to={`/articles/topics/${article.topic}`}>{article.topic}</Link>
+        <p>{article.author}</p>
+      </li>
+    </Link>
   );
 };
-
-// const StudentCard = ({ student }) => {
-//   return (
-//     <Link to={`/students/${student._id}`}>
-//       <li key={student._id}>
-//         <span className="name">{student.name}</span>
-//         <br />
-//         Current block: {student.currentBlock}
-//         <br />
-//         Starting cohort: {student.startingCohort}
-//       </li>
-//       <br />
-//     </Link>
-//   );
-// };
 
 export default ArticleCard;
