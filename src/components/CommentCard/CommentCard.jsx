3c55242446e204a9776dc8f6075b2aaef8ea69a6
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-17 14:08:22 +0100

    added deleteComment function

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import styles from "./CommentCard.module.css";
+import * as api from "../../api";
 
-const CommentCard = ({ comment }) => {
-  return (
-    <li className={styles.commentCard}>
-      <p>{comment.body}</p>
-      <p>{comment.author}</p>
-    </li>
-  );
-};
+class CommentCard extends React.Component {
+  render() {
+    const { comment } = this.props;
+    return (
+      <li className={styles.commentCard}>
+        <p>{comment.body}</p>
+        <p>{comment.author}</p>
+        <button onClick={() => this.deleteComment(comment.comment_id)}>
+          Delete
+        </button>
+      </li>
+    );
+  }
+
+  deleteComment = comment_id => {
+    api.deleteCommentById(comment_id);
+    this.props.removeComment(comment_id);
+  };
+}
 
 export default CommentCard;
