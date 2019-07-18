ecbf7bd713d5fc4669de0c77bacd16dbd5703a6e
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-18 16:46:26 +0100

    adding error handling

--- src/components/ArticleComments/ArticleComments.jsx
+++ src/components/ArticleComments/ArticleComments.jsx
@@ -3,14 +3,18 @@ import CommentCard from "../CommentCard/CommentCard";
 import styles from "./ArticleComments.module.css";
 import * as api from "../../api";
 import CommentAdder from "../CommentAdder";
+import ErrorPage from "../ErrorPage";
 
 class ArticleComments extends React.Component {
   state = {
-    comments: []
+    comments: [],
+    err: null
   };
 
   render() {
-    const { comments } = this.state;
+    const { comments, err } = this.state;
+    if (err) return <ErrorPage err={err} />;
+
     return (
       <div className={styles.commentsList}>
         <h3>What is news without comments...</h3>
