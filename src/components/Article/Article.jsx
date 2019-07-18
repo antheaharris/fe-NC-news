2c716c66eccaf46465576c5a930e7dc781429d35
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-18 16:15:41 +0100

    linked error page up for error handling

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,16 +3,20 @@ import * as api from "../../api";
 import ArticleComments from "../ArticleComments/ArticleComments";
 import styles from "./Article.module.css";
 import Voter from "../Voter/Voter";
+import ErrorPage from "../ErrorPage";
 
 class Article extends React.Component {
   state = {
     article: {},
-    loading: true
+    loading: true,
+    err: null
   };
 
   render() {
     const { title, body, votes, article_id } = this.state.article;
-    const { loading } = this.state;
+    const { loading, err } = this.state;
+    if (err) return <ErrorPage err={err} />;
+
     if (loading) {
       return <p>great articles take time...</p>;
     }
