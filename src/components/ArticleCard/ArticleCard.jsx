dcb142d874dc8ea2a7d46642d0e5f7d24daae8a8
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-18 16:18:11 +0100

    updated article card to show the number of comments on that article

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -3,13 +3,14 @@ import { Link } from "@reach/router";
 import styles from "./ArticleCard.module.css";
 import Voter from "../Voter/Voter";
 
-const ArticleCard = ({ article_id, title, topic, votes }) => {
+const ArticleCard = ({ article_id, title, topic, votes, comment_count }) => {
   return (
     <li className={styles.articleCard}>
       <Link to={`/articles/${article_id}`}>
         <h3>{title}</h3>{" "}
       </Link>
       <Link to={`/articles/topics/${topic}`}>{topic}</Link>
+      <p>Comments {comment_count}</p>
       <Voter type={"article"} votes={votes} id={article_id} />
     </li>
   );
