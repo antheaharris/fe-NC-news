a27a6c557795a8a0ec876e6dd884810abcbe9d92
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-18 16:17:08 +0100

    edited text on error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const ErrorPage = ({ err }) => {
   return (
     <div>
-      <h1>{err.message}</h1>
+      <h1>Whoops! Something went wrong...</h1>
+      <h2>{err.message}</h2>
     </div>
   );
 };
