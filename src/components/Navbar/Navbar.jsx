6433e9de391e27900e236aba29ff918e82b11ad3
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-19 13:05:51 +0100

    styling with flex

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "@reach/router";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav className="app-navbar">
-      <Link to="/">Home</Link>
-      <Link to="/articles">Articles</Link>
+    <nav className={styles.container}>
+      <Link to="/" className={styles.home}>
+        Home
+      </Link>
+      <Link to="/articles" className={styles.articles}>
+        Articles
+      </Link>
     </nav>
   );
 };
