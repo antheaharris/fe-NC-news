da35681b0ec59c892d5a3d2cd17bd54f9d01916a
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-19 13:06:27 +0100

    styling with grid

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="app-header">
-      <h1 className="app-title">NC Newsbeat</h1>
-      <Navbar />
+    <header className={styles.container}>
+      <h1 className={styles.item_a}>NC Newsbeat</h1>
+      <Navbar className={styles.item_b} />
     </header>
   );
 };
