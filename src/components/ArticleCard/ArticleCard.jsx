b687d2c9379df0c7d8a8a0f175baf86113f35d1e
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-22 09:20:43 +0100

    added in user friendly date format for article and comment cards

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -2,16 +2,34 @@ import React from "react";
 import { Link } from "@reach/router";
 import styles from "./ArticleCard.module.css";
 import Voter from "../Voter/Voter";
+const distanceInWords = require("date-fns/distance_in_words_to_now");
 
-const ArticleCard = ({ article_id, title, topic, votes, comment_count }) => {
+const ArticleCard = ({
+  article_id,
+  title,
+  topic,
+  votes,
+  comment_count,
+  author,
+  created_at
+}) => {
+  let formattedDate = distanceInWords(new Date(created_at));
   return (
     <li className={styles.articleCard}>
       <Link to={`/articles/${article_id}`}>
-        <h3>{title}</h3>{" "}
+        <h3>{title}</h3>
       </Link>
+      <p>
+        written by {author} {formattedDate} ago
+      </p>
       <Link to={`/articles/topics/${topic}`}>{topic}</Link>
       <p>Comments {comment_count}</p>
-      <Voter type={"article"} votes={votes} id={article_id} />
+      <Voter
+        type={"article"}
+        votes={votes}
+        id={article_id}
+        className={styles.flex_vote}
+      />
     </li>
   );
 };
--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import styles from "./CommentCard.module.css";
 import * as api from "../../api";
 import Voter from "../Voter/Voter";
+const distanceInWords = require("date-fns/distance_in_words_to_now");
 
 class CommentCard extends React.Component {
   render() {
-    const { body, author, comment_id, votes } = this.props.comment;
+    const { body, author, comment_id, votes, created_at } = this.props.comment;
+    let formattedDate = distanceInWords(new Date(created_at));
     return (
       <li className={styles.commentCard}>
         <p>{body}</p>
-        <p>{author}</p>
+        <p>
+          posted by {author} {formattedDate} ago
+        </p>
         <Voter type={"comment"} votes={votes} id={comment_id} />
         <button onClick={() => this.deleteComment(comment_id)}>Delete</button>
       </li>
