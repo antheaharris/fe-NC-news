9dab518e8e8679cbe53784d192617daf6d8b15ac
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-22 12:12:20 +0100

    using error page component to handle a bad URL error

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 const ErrorPage = ({ err }) => {
-  return (
-    <div>
-      <h1>Whoops! Something went wrong...</h1>
-      <h2>{err.message}</h2>
-    </div>
-  );
+  if (err) {
+    return (
+      <div>
+        <h1>Whoops! Something went wrong...</h1>
+        <h2>{err.message}</h2>
+      </div>
+    );
+  } else {
+    return <h1>Whoops! Something went wrong...</h1>;
+  }
 };
 
 export default ErrorPage;
