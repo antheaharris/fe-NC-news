d3ef6b1aaab18d0bc046baf231052e6e8b810ae5
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-08-05 08:26:44 +0100

    styling & spacing for topics on the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styles from "./Navbar.module.css";
+import * as api from "../../api";
+import ErrorPage from "../ErrorPage";
 
-const Navbar = () => {
-  return (
-    <nav className={styles.container}>
-      <Link to="/" className={styles.articles}>
-        Articles
-      </Link>
-    </nav>
-  );
-};
+class Navbar extends React.Component {
+  state = {
+    topics: [],
+    err: null
+  };
+
+  render() {
+    const { topics, err } = this.state;
+    if (err) return <ErrorPage err={err} />;
+    return (
+      <nav className={styles.flex_container}>
+        <section>
+          <Link to="/">articles</Link>
+        </section>
+
+        <section className={styles.topics_box}>
+          <span className={styles.topic_slug}>I want to read about</span>
+          {topics.map(topic => {
+            return (
+              <Link to={`/articles/topics/${topic.slug}`}>
+                <span className={styles.topic_slug}>{topic.slug}</span>
+              </Link>
+            );
+          })}
+        </section>
+      </nav>
+    );
+  }
+
+  componentDidMount = () => {
+    api
+      .getTopics()
+      .then(topics => {
+        this.setState({ topics });
+      })
+      .catch(err => {
+        this.setState({ err });
+      });
+  };
+}
 
 export default Navbar;
