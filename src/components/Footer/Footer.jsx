90fc45e7176f2d2aa7d690e4540ad6cb6ea61685
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-08-05 14:19:48 +0100

    moved file location

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <section className={styles.footer}>
+      <footer>
+        <h3>Anthea Harris 2019</h3>
+      </footer>
+    </section>
+  );
+};
+
+export default Footer;
