8ada7028519349ab3ea831801082495de494ea04
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-08-05 12:51:11 +0100

    css styling added, font, colours & layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,15 @@ const Header = ({ loggedInUser }) => {
   return (
     <header className={styles.flex_container}>
       <section className={styles.flex_title}>
-        <h1>NC Newsbeat</h1>
-      </section>
-
-      <section className={styles.flex_navbar}>
-        <Navbar />
+        <h1>NC NEWS</h1>
       </section>
 
       <section className={styles.flex_user}>
         <h5>logged in as {loggedInUser}</h5>
+      </section>
+
+      <section className={styles.flex_navbar}>
+        <Navbar />
       </section>
     </header>
   );
